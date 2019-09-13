dc6f098873db9d443504845a636ac545fd07db9a
Bailey Beck <baileymbeck@gmail.com>
2019-09-13 18:01:20 -0500

    added win img and reset css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,9 @@
-$(".btn").click(function(){
-    console.log(this);
-  });
+// ----- pseudo code -----
+// 7 multi choice questions
+// set timer for 60 seconds
+//  when answer is selected it changes color and is saved (to be grade when the time finishes)
+
+//  ---- when timer finishes ----
+//  set function to determine correct answers
+//  display correct answer in GREEN
+// display fist bump image in top right
